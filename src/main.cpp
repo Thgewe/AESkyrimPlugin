31c2e730697d1ce0119f4129c922b9abe500b386
Viktor Hanhatov <vah12@unigine.com>
2026-05-21 02:44:17 +0700

    init plugin build

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <SKSE/SKSE.h>
+
+SKSEPluginVersion = []() noexcept {
+	SKSE::PluginVersionData v{};
+	v.PluginVersion({1, 0, 0});
+	v.PluginName("AESkyrimPlugin");
+	v.AuthorName("Thgewe");
+	v.UsesAddressLibrary();
+	v.UsesUpdatedStructs();
+	v.CompatibleVersions({SKSE::RUNTIME_SSE_LATEST});
+	return v;
+}();
+
+SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+	SKSE::Init(skse);
+	SKSE::log::info("loaded!");
+	return true;
+}
