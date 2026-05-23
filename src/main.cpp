860be21ce1425f23621b233f3c3e70bb09f0208f
Viktor Hanhatov <vah12@unigine.com>
2026-05-23 10:26:19 +0700

    SKSE plugin template

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 SKSEPluginVersion = []() noexcept {
 	SKSE::PluginVersionData v{};
-	v.PluginVersion({1, 0, 0});
+	v.PluginVersion({ 1, 0, 0 });
 	v.PluginName("AESkyrimPlugin");
 	v.AuthorName("Thgewe");
 	v.UsesAddressLibrary();
 	v.UsesUpdatedStructs();
-	v.CompatibleVersions({SKSE::RUNTIME_SSE_LATEST});
+	v.CompatibleVersions({ SKSE::RUNTIME_SSE_LATEST });
 	return v;
 }();
 
-SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+SKSEPluginLoad(const SKSE::LoadInterface* skse)
+{
 	SKSE::Init(skse);
 	logger::info("loaded!");
 	return true;
