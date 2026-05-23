74d168b43f03c85c80d6e7a6ccf53c8c3cc236be
Viktor Hanhatov <vah12@unigine.com>
2026-05-23 21:38:03 +0700

    add ActivateSink example

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,33 @@
+class ActivateSink : public RE::BSTEventSink<RE::TESActivateEvent>
+{
+public:
+	static ActivateSink* GetSingleton()
+	{
+		static ActivateSink instance;
+		return &instance;
+	}
+
+	RE::BSEventNotifyControl ProcessEvent(
+		const RE::TESActivateEvent*               a_event,
+		RE::BSTEventSource<RE::TESActivateEvent>* a_eventSource) override
+	{
+
+		if (a_event && a_event->objectActivated && a_event->actionRef)
+		{
+			logger::info("{} activated {}",
+				a_event->actionRef->GetDisplayFullName(), a_event->objectActivated->GetDisplayFullName());
+		}
+
+		return RE::BSEventNotifyControl::kContinue;
+	};
+
+private:
+	ActivateSink() = default;
+	~ActivateSink() override = default;
+	ActivateSink(const ActivateSink&) = delete;
+	ActivateSink& operator=(const ActivateSink&) = delete;
+};
+
 SKSEPluginVersion = []() noexcept {
 	SKSE::PluginVersionData v{};
 	v.PluginVersion({ 1, 0, 0 });
@@ -13,5 +43,13 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse)
 {
 	SKSE::Init(skse);
 	logger::info("loaded!");
+
+	SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* msg){
+		if (msg && msg->type == SKSE::MessagingInterface::kDataLoaded) {
+			RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESActivateEvent>(ActivateSink::GetSingleton());
+			logger::info("Activate sink registered");
+		}
+	});
+
 	return true;
 }
